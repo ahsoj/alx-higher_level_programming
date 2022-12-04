98a824dd7150168548f7024e5e7fb75f1210c714
ahsoj <103538218+ahsoj@users.noreply.github.com>
2022-12-04 09:58:31 +0000

    Task_0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,12 @@
 #!usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+from add_0 import add
+
+
+def adds():
     a = 1
     b = 2
     print("{} + {} = {}".format(str(a), str(b), str(add(a, b))))
+
+
+if __name__ == "__main__":
+    adds()
