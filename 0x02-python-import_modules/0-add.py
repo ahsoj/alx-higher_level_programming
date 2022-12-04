390524a6a0c6ad020de4aec73330141f440a472a
ahsoj <103538218+ahsoj@users.noreply.github.com>
2022-12-04 08:38:18 +0000

    Task_1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,12 @@
 #!usr/bin/python3
+# from add_0 import add
+# if __name__ == "__main__":
+#     a = 1
+#     b = 2
+#     print("{:s} + {:s} = {:s}".format(str(a), str(b), str(add(a, b))))
+
 from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{:s} + {:s} = {:s}".format(str(a), str(b), str(add(a, b))))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
