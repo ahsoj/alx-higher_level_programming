d59a53870b3b7f830241f8be88929c919fbfecaa
ahsoj <103538218+ahsoj@users.noreply.github.com>
2022-12-04 08:14:57 +0000

    Task_0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!usr/bin/python3
 from add_0 import add
-if __name__ == "__main__":
-    a = 1
-    b = 2
-    print("{:s} + {:s} = {:s}".format(str(a), str(b), str(add(a, b))), end="\n")
+a = 1
+b = 2
+print("{:s} + {:s} = {:s}".format(str(a), str(b), str(add(a, b))), end="\n")
