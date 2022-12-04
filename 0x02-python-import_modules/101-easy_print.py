af61772a2bcb1f2c5f23cebff06315892565a82e
ahsoj <103538218+ahsoj@users.noreply.github.com>
2022-12-04 11:19:19 +0000

    Task_101_A

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    __builtins__.__dict__['p,r,i,n,t'.replace(',', '')]('#pythoniscool')
+__builtins__.__dict__['p,r,i,n,t'.replace(',', '')]('#pythoniscool')
