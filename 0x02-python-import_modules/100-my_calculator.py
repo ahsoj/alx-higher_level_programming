6ff94933d7a7fa3ce6877072a64437780c0675fd
ahsoj <103538218+ahsoj@users.noreply.github.com>
2022-12-04 10:36:13 +0000

    task_100_A

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,32 @@
-#1/usr/bin/pyton3
+#!/usr/bin/pyton3
 
 import sys
-nu = len(sys.argv)
-mu = list(sys.argv)
-
-a = int(mu[1])
-b = int(mu[3])
-
 from calculator_1 import add, sub, mul, div
 
-if nu < 3:
+nu = len(sys.argv)
+mu = list(sys.argv)
+if len(mu) < 2:
     print("Usage: {} <a> <operator> <b>".format(mu[0]), end="\n")
-elif mu[2] == "+":
-    res = add(a, b)
-    print("{} + {} = {}".format(a, b, res), end="\n")
-elif mu[2] == "-":
-    res = sub(a, b)
-    print("{} - {} = {}".format(a, b, res), end="\n")
-elif mu[2] == "*":
-    res = mul(a, b)
-    print("{} * {} = {}".format(a, b, res), end="\n")
-elif mu[2] == "/":
-    res = div(a, b)
-    print("{} / {} = {}".format(a, b, res), end="\n")
 else:
-    print("Unknown operator. Activable operators: +, -, * and / ", end="\n")
+    a = int(mu[1])
+    b = int(mu[3])
+
+    def calcsec():
+        if mu[2] == "+":
+            res = add(a, b)
+            print("{} + {} = {}".format(a, b, res), end="\n")
+        elif mu[2] == "-":
+            res = sub(a, b)
+            print("{} - {} = {}".format(a, b, res), end="\n")
+        elif mu[2] == "*":
+            res = mul(a, b)
+            print("{} * {} = {}".format(a, b, res), end="\n")
+        elif mu[2] == "/":
+            res = div(a, b)
+            print("{} / {} = {}".format(a, b, res), end="\n")
+        else:
+            print("Unknown operator. Activable operators:", end=" ")
+            print(" +, -, * and /")
+
+    if __name__ == "__main__":
+        calcsec()
