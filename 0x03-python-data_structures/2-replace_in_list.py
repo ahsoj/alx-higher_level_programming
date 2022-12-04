a0536f54bd94ba7eba206cce74d41feb9ddb349d
ahsoj <103538218+ahsoj@users.noreply.github.com>
2022-12-04 14:24:51 +0000

    Task_2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
+
+
+if __name__ == "__main__":
+    replace_in_list()
