f2c359e6fed2b22f7154889c00973cd5ab02a785
ahsoj <103538218+ahsoj@users.noreply.github.com>
2022-12-04 13:43:49 +0000

    Task_1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,7 +4,11 @@
 def element_at(my_list, idx):
     if idx < 0:
         pass
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         pass
     else:
         return my_list[idx]
+
+
+if __name__ == "__main__":
+    element_at()
