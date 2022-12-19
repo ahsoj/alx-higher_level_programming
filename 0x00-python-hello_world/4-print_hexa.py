4717cc4fd68a8b9f45c5439939317c28f18ae355
ahsoj <eyasuchaka259@gmail.com>
2022-12-19 11:23:18 -0800

    0x05. Python - Exceptions

--- 0x00-python-hello_world/4-print_hexa.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-
-for i in range(0, 99):
-    print("{:d} = {:s}".format(i, hex(i)), end="\n")
--- 0x01-python-if_else_loops_functions/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    """print now"""
-    import sys
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
