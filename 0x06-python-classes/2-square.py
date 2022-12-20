25ceb2fe9c2a3a1aa007930e46fe8d952e362045
ahsoj <103538218+ahsoj@users.noreply.github.com>
2022-12-20 16:29:38 +0000

    2_Square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,16 +5,20 @@
 class Square:
     """represent class square"""
     def __init__(self, size=0):
-        """check for type"""
+        """check for data type"""
 
         if isinstance(size, int):
 
             """check for negative"""
             if size >= 0:
                 self._Square__size = size
+            """if not true"""
             else:
                 print("size must be >= 0")
+                """raise error message"""
                 raise ValueError
+        """if not true"""
         else:
             print("size must be an integer")
+            """raise error message"""
             raise TypeError
