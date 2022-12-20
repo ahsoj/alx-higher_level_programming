c6a581b4f3ac2dc0fbe64020898124fd04a3a960
ahsoj <103538218+ahsoj@users.noreply.github.com>
2022-12-20 15:51:57 +0000

    1_square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """represent square"""
+    def __init__(self, size):
+        """Private instance attribute"""
+        self._Square__size = size
