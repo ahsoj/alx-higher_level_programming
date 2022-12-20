f19c9e2617af37f053e24a0e6f81400af1852862
ahsoj <103538218+ahsoj@users.noreply.github.com>
2022-12-20 19:33:34 +0000

    6-square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,23 +3,21 @@ class Square:
     """represent class square"""
     def __init__(self, size=0, position=(0, 0)):
         self._Square__size = size
-        if isinstance(position, tuple):
-            self._Square__position = position
-        else:
-            print("position must be a tuple of 2 positive integers", end="")
-            """raise error message"""
-            raise TypeError
-    
+        self._Square__position = position
+            
     @property
     def position(self):
         return self._Square__position
 
     @position.setter
     def position(self, value):
-        if len(value) >= 2 and value > 0:
-            self._Square__position = value
-        print("position must be a tuple of 2 positive integers")
-        raise TypeError
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            print("position must be a tuple of 2 positive integers")
+            raise TypeError
+        self.__position = value
     
     @property
     def size(self):
@@ -30,12 +28,15 @@ class Square:
         print(self._Square__size * self._Square__size)
     
     def my_print(self):
-        if self._Square__size == 0:
-            print("\n")
-        for i in range(self._Square__size):
-            for j in range(self._Square__size):
-                print("#", end="")
-            print()
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
     
     @size.setter
     def size(self, value):
