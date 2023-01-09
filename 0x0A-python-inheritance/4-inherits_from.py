598b09afe14cc40d0565073f716990e2563aea4a
ahsoj <103538218+ahsoj@users.noreply.github.com>
2023-01-09 11:40:49 +0000

    python-inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define `inheritance_from` function"""
+
+
+def inherits_from(obj, a_class):
+    """Write a function that returns True if the object is \
+        an instance of a class that inherited \
+        (directly or indirectly) from the specified class ; otherwise False."""
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
