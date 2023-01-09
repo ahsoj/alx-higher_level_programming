5932f909cbae23fc8cdbbd697a3dc262dd10163b
ahsoj <103538218+ahsoj@users.noreply.github.com>
2023-01-09 12:59:02 +0000

    python-inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Use ``BaseGeometry` in `Rectangle` class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Define `Rectangle` class"""
+
+    def __init__(self, size):
+        """Instantiation square with `size` """
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """print square of size"""
+        return self.__size * self.__size
