bc6ed0220583ce0d80910a08796010fd898c2010
ahsoj <103538218+ahsoj@users.noreply.github.com>
2023-01-10 11:55:44 +0000

    python-I/O

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" returns the dictionary description with simple data structure """
+
+
+def class_to_json(obj):
+    """Return the dictionary description"""
+    return obj.__dict__
