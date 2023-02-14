7f6f57ec6be53f249da9a5a1de1f1426499b8687
eyasucC <eyasuchaka129@gmail.com>
2023-02-14 03:08:16 -0800

    second_table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- the second table in db
+DROP TABLE IF EXISTS `second_table`;
+FLUSH TABLES `second_table`;
+CREATE TABLE second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
