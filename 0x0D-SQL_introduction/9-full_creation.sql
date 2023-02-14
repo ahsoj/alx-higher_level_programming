98865812a503448de3b4552c5638206b07cd860d
eyasucC <eyasuchaka129@gmail.com>
2023-02-14 03:14:54 -0800

    sql_second_table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -8,7 +8,7 @@ CREATE TABLE second_table(
 );
 
 INSERT INTO second_table (id, name, score)
-VALUES (1, 'John', 10),
-	(2, 'Alex', 3),
-	(3, 'Bob', 14),
-	(4, 'George', 8);
+VALUES (1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
