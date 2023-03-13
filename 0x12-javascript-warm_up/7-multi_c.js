d07fe5d7e3a77229392c44a287aecbc2cd989b07
eyasu chaka <eyasuchaka259@gmail.com>
2023-03-13 08:19:42 +0000

    multi_c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const processArgv = process.argv.slice(2);
+
+if (!processArgv[0]) {
+	console.log('Missing number of occurrences');
+} else if (processArgv[0] > 0) {
+	for (let i = 1; i <= processArgv; i++) {
+		console.log('C is fun');
+	}
+} else {
+	return;
+}
