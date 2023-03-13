ceae786462043ba76e056c50c35cd52802e7b75b
eyasu chaka <eyasuchaka259@gmail.com>
2023-03-13 07:32:58 +0000

    value_argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const processArgv = process.argv.slice(2);
+
+if (processArgv[0] === undefined) {
+	console.log('No argument');
+} else {
+	for (let i of processArgv) {
+		console.log(i);
+	}
+}
