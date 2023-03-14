25c66459ccd644ef7871f98eab0bb175a88929d8
eyasu chaka <eyasuchaka259@gmail.com>
2023-03-14 12:41:03 +0000

    4-rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,27 +1,30 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-        this.width = w;
-	this.height = h;
-    }
-    print () {
-        for (let i = 1; i <= this.height;i++) {
-            for (let j = 1; j <= this.width; j++) {
-                process.stdout.write('X'+'')
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 1; i <= this.height; i++) {
+      for (let j = 1; j <= this.width; j++) {
+        process.stdout.write('X' + '');
 	    }
 	    console.log();
-        }
     }
-    rotate () {
-    	let ex = this.width;
-	this.width = this.height;
-	this.height = ex;
-    }
-    double () {
-        this.width *= 2;
-        this.height *= 2;
-    }
+  }
+
+  rotate () {
+    	const ex = this.width;
+    this.width = this.height;
+    this.height = ex;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
