1e1cb1501a034f229d0d22de736943df523432f2
eyasucC <eyasuchaka129@gmail.com>
2023-03-16 05:38:12 -0700

    My_filter_states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Create mysql cursor connector for selectings item from tables"""
+
+import MySQLdb
+import sys
+
+
+def main(argvs):
+    """
+     takes in an argument and displays all values in the \
+     states table of hbtn_0e_0_usa where name matches \
+     the argument.
+    """
+
+    conn = MySQLdb.connect(
+            host="localhost", 
+            port=3306, 
+            user=f"{argvs[0]}", 
+            passwd=f"{argvs[1]}", 
+            db=f"{argvs[2]}", 
+            charset="utf8"
+        )
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT * FROM states 
+        WHERE name = '{}' 
+        ORDER BY states.id ASC """.format(argvs[3]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
