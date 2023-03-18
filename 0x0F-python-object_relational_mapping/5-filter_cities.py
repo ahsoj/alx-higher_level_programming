97165d1a832b043ed936dbf4b3552cfe1001a35f
eyasucC <eyasuchaka129@gmail.com>
2023-03-18 01:56:31 -0700

    filter_cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,21 +10,24 @@ def main(argvs):
     """
 
     conn = MySQLdb.connect(
-            host="localhost", 
-            port=3306, 
-            user=f"{argvs[0]}", 
-            passwd=f"{argvs[1]}", 
-            db=f"{argvs[2]}", 
+            host="localhost",
+            port=3306,
+            user=f"{argvs[0]}",
+            passwd=f"{argvs[1]}",
+            db=f"{argvs[2]}",
             charset="utf8"
         )
     cur = conn.cursor()
     cur.execute("""
-        SELECT cities.id, cities.name, states.name FROM cities \
-        LEFT JOIN states ON cities.state_id = states.id  
-        ORDER BY cities.id ASC """)
+        SELECT cities.name FROM cities \
+        LEFT JOIN states ON cities.state_id = states.id \
+        WHERE states.name = %s \
+        ORDER BY cities.id ASC """, (argvs[3],))
     query_rows = cur.fetchall()
+    ct = []
     for row in query_rows:
-        print(row)
+        ct.append("".join(row))
+    print(", ".join(c for c in ct))
     cur.close()
     conn.close()
 
