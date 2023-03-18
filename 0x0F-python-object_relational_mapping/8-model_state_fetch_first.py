16161c6b5a9de98ba4d20734edac39c3e447c24f
eyasucC <eyasuchaka129@gmail.com>
2023-03-18 05:18:04 -0700

    fetch_one

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Create sqlalchemy fetcher selecting item from tables"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+def main(engine):
+    """
+    """
+    Session = sessionmaker(bind=engine)
+    query = Session().query(State).order_by(State.id.asc()).first()
+    if query == None:
+        print("Nothing")
+    print(query)
+    # for q in query:
+    #    print(q)
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+
+    engine = create_engine(
+            'mysql://{}:{}@localhost:3306/{}'.format(
+                argv[1], argv[2], argv[3]
+                ))
+    # Base.metadata.create_all(engine)
+    main(engine)
