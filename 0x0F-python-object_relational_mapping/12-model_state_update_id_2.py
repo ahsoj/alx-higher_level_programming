8f62df236686ac14ade98ba9d5ce5c372c6c5f88
eyasucC <eyasuchaka129@gmail.com>
2023-03-18 07:05:09 -0700

    state_update

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Create sqlalchemy fetcher selecting item from tables"""
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+def main(engine):
+    """
+        return item with \
+                `fetch_one` method
+        rtype: obj
+    """
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    user = session.query(State).filter_by(id=2).first()
+    if user:
+        user.name = "New Mexico"
+        session.commit()
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+
+    engine = create_engine(
+            'mysql://{}:{}@localhost:3306/{}'.format(
+                argv[1], argv[2], argv[3]
+                ))
+    # Base.metadata.create_all(engine)
+    main(engine)
