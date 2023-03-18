e9c0d2df0ef67f4fb7069e33f11dbcd4ef9ba352
eyasucC <eyasuchaka129@gmail.com>
2023-03-18 01:22:20 -0700

    cities_by_state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,11 +11,11 @@ def main(argvs):
     """
 
     conn = MySQLdb.connect(
-            host="localhost", 
-            port=3306, 
-            user=f"{argvs[0]}", 
-            passwd=f"{argvs[1]}", 
-            db=f"{argvs[2]}", 
+            host="localhost",
+            port=3306,
+            user=f"{argvs[0]}",
+            passwd=f"{argvs[1]}",
+            db=f"{argvs[2]}",
             charset="utf8"
         )
     cur = conn.cursor()
