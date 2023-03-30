a8108a5159201734dd60649f1fd8046b2250afab
eyasu chaka <daa282386@gmaill.com>
2023-03-30 03:30:24 -0700

    python_network

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send POST request to URL with data ...
-curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" $1
+curl -X POST -F "email=test@gmail.com" -F "subject=I will always be here for PLD" $1
