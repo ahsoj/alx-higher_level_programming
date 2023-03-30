0f89d431e094998d09fab6052f4cb6a75c108403
eyasu chaka <daa282386@gmaill.com>
2023-03-30 02:32:42 -0700

    python-netwrok

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# send a request to url ...
-# display the size of response body with byte
-
+# send a request to url & display byte data ...
 curl -G -s -o /dev/null -w '%{size_download}\n' $1
