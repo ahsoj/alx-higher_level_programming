8a4eb5b02d19198748b5ab7335cc1b35bb147599
eyasu chaka <daa282386@gmail.com>
2023-03-31 11:35:26 -0700

    hbtn_stataus

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
+"""
+     Python script that fetches\
+             https://alx-intranet.hbtn.io/status
+"""
 import requests
 
 
-req = requests.get("https://intranet.hbtn.io/status")
-sp = ' '
-print("Body response:")
-print(f"{sp*4}- type: {type(req.text)}")
-print(f"{sp*4}- content: {req.text}")
+def main():
+    """
+        fetch `https://alx-intranet.hbtn.io/status`
+        rtype: str
+    """
+    req = requests.get("https://intranet.hbtn.io/status")
+    sp = ' '
+    print("Body response:")
+    print(f"{sp*4}- type: {type(req.text)}")
+    print(f"{sp*4}- content: {req.text}")
+
+
+if __name__ == "__main__":
+    main()
