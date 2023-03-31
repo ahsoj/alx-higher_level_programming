7eb416e64c307bc726ef7d24cc0976ad30dccf3a
eyasu chaka <daa282386@gmail.com>
2023-03-31 09:13:11 -0700

    fetch_status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Resource Fetching function"""
+import urllib.request
+
+
+def main():
+    """ fetches https://alx-intranet.hbtn.io/status """
+    with urllib.request.urlopen(
+            "https://alx-intranet.hbtn.io/status") as read_url:
+        url = read_url.read()
+        utf = str(url).rsplit("'")[1]
+        print(f"Body response:\n\t- type: {type(url)} \
+                \n\t- content: {url}\n\t- utf8 content: {utf}")
+
+
+if __name__ == "__main__":
+    main()
