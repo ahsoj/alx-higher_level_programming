4282652926c271bda108dc4e354a518cbbc449a4
eyasu chaka <daa282386@gmail.com>
2023-03-31 09:40:55 -0700

    fetch_status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,10 +7,13 @@ def main():
     """ fetches https://alx-intranet.hbtn.io/status """
     with urllib.request.urlopen(
             "https://alx-intranet.hbtn.io/status") as read_url:
-        url = read_url.read() 
+        url = read_url.read()
+        sp = ' '
         utf = str(url).rsplit("'")[1]
-        print(
-        f"Body response:\n\t- type: {type(url)} \n\t- content: {url}\n\t- utf8 content: {utf}")
+        print(f"Body response:")
+        print(f"{sp*4}- type: {type(url)}")
+        print(f"{sp*4}- content: {url}")
+        print(f"{sp*4}- utf8 content: {utf}")
 
 
 if __name__ == "__main__":
