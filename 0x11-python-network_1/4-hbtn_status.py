76518cd1d5de1b637f514ab47e4754c8fcc0e112
eyasu chaka <daa282386@gmail.com>
2023-03-31 11:31:13 -0700

    hbtn_status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,8 @@
 import requests
 
 
-if __name__ == "__main__":
-    req = requests.get("https://intranet.hbtn.io/status")
-    sp = ' '
-    print("Body response:")
-    print(f"{sp*4}- type: {type(req.text)}")
-    print(f"{sp*4}- content: {req.text}")
+req = requests.get("https://intranet.hbtn.io/status")
+sp = ' '
+print("Body response:")
+print(f"{sp*4}- type: {type(req.text)}")
+print(f"{sp*4}- content: {req.text}")
