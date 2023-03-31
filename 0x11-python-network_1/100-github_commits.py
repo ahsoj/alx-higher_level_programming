d44ea7bb1e2429a524a3a21f5405465d88c0720e
eyasu chaka <daa282386@gmail.com>
2023-03-31 12:29:17 -0700

    github_commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+"""
+import requests
+
+
+def main(arg1, arg2):
+    """
+        Python script that takes 2 arguments in \
+                order to solve this challenge.
+    """
+    url = f"https://api.github.com/repos/{arg1}/{arg2}/commits"
+
+    req = requests.get(url)
+    commits = req.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
+
+
+if __name__ == "__main__":
+    from sys import argv
+    main(argv[1], argv[2])
