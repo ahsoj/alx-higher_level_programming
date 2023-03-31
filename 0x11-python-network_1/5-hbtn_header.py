035e30a60acf6ef36558f532aeca93788460fb43
eyasu chaka <daa282386@gmail.com>
2023-03-31 11:45:35 -0700

    hbtn_header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+    Python script that takes in a URL, sends a request \
+        to the URL and displays the value of the variable
+ """
 
-from sys import argv
-import requests
 
-req = requests.get(argv[1])
-print(req.headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    req = requests.get(argv[1])
+    print(req.headers.get("X-Request-Id"))
