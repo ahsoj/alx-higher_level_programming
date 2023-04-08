ae02d4932bab6c90aa4ccc29cd3500a6c585df2d
eyasu chaka <daa282386@gmail.com>
2023-04-08 14:01:23 -0700

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    if (ord(c) >= ord('a') and ord(c) <= ord('z')):
+    if ord(c) > 96 and ord(c) < 123:
         return True
     return False
-if __name__ == "__main__":
-     islower()
