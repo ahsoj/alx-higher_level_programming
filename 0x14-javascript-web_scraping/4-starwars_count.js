78b2cd0bdd0a75eb738a01a302ead134857cfda1
eyasu chaka <daa282386@gmail.com>
2023-04-25 14:27:49 +0000

    starwars_count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body).results;
+    let __count = 0;
+    for (const index in responseJSON) {
+      const res = responseJSON[index].characters;
+      for (const _index in res) {
+        if (index[_index].includes('18')) {
+          __count++;
+        }
+      }
+    }
+    console.log(__count);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
